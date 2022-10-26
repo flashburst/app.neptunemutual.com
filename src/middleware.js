4ef150c2053b671436deb717ee5f8227436b2b84
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-26 16:31:07 +0530

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,46 @@
-import { handleBuildManifest, handleHtmlPages } from '@/src/middleware/handlers'
 import { NextResponse } from 'next/server'
+
+/** @type string */
+const regions = process.env.NEXT_PUBLIC_UNSUPPORTED_REGIONS || ''
+
+const unavailableTo = regions.split(',').filter((x) => !!x)
+
+/**
+ *
+ * @param {import("next/server").NextRequest} req
+ * @returns {Promise<Response | undefined> | Response | undefined}
+ */
+function handleBuildManifest (req) {
+  if (req.url.includes('buildManifest')) {
+    const response = NextResponse.rewrite(new URL('/buildManifest.js', req.url))
+    response.headers.set('Access-Control-Allow-Origin', 'null')
+    return response
+  }
+}
+
+/**
+ *
+ * @param {import("next/server").NextRequest} req
+ * @returns {Promise<Response | undefined> | Response | undefined}
+ */
+function handleHtmlPages (req) {
+  const isHTMLPage = typeof req.headers.get('accept') === 'string' && (req.headers.get('accept').includes('text/html') || req.headers.get('accept').includes('application/xhtml+xml'))
+
+  const country = req.geo?.country || ''
+
+  if (!isHTMLPage || !country || unavailableTo.length === 0) {
+    return
+  }
+
+  const unavailable = unavailableTo.indexOf(country) > -1
+  const landingPage = req.nextUrl.clone().pathname === '/unavailable'
+
+  if (unavailable && !landingPage) {
+    const response = NextResponse.rewrite(new URL('/unavailable', req.url), { status: 451 })
+    response.headers.set('Access-Control-Allow-Origin', 'null')
+    return response
+  }
+}
 
 /**
  *
