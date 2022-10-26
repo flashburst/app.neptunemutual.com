38d01afe48cf37ebeb1b5c8bae965f71922b496b
flashburst <82953782+flashburst@users.noreply.github.com>
2022-10-26 16:21:05 +0530

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,5 @@
+import { handleBuildManifest, handleHtmlPages } from '@/src/middleware/handlers'
 import { NextResponse } from 'next/server'
-
-/** @type string */
-const regions = process.env.NEXT_PUBLIC_UNSUPPORTED_REGIONS || ''
-
-const unavailableTo = regions.split(',').filter((x) => !!x)
 
 /**
  *
@@ -11,33 +7,19 @@ const unavailableTo = regions.split(',').filter((x) => !!x)
  * @returns {Promise<Response | undefined> | Response | undefined}
  */
 export function middleware (req) {
-  console.log('middleware', req.url)
-  // console.log('middleware', req.method)
-  // console.log('middleware', req.geo)
+  let response = handleBuildManifest(req)
 
-  if (req.url.includes('buildManifest')) {
-    // return NextResponse.next()
-    // return NextResponse.json({ message: 'Auth required' }, { status: 401 })
-    return NextResponse.rewrite(new URL('/buildManifest.js', req.url))
-  }
-
-  const country = req.geo?.country || ''
-
-  if (!country || unavailableTo.length === 0) {
-    const response = NextResponse.next()
-    response.headers.set('Access-Control-Allow-Origin', 'null')
+  if (response) {
     return response
   }
 
-  const unavailable = unavailableTo.indexOf(country) > -1
-  const landingPage = req.nextUrl.clone().pathname === '/unavailable'
+  response = handleHtmlPages(req)
 
-  if (unavailable && !landingPage) {
-    return NextResponse.rewrite(new URL('/unavailable', req.url), { status: 451 })
-    // return NextResponse.redirect(new URL('/unavailable', req.url))
+  if (response) {
+    return response
   }
 
-  const response = NextResponse.next()
+  response = NextResponse.next()
   response.headers.set('Access-Control-Allow-Origin', 'null')
   return response
 }
@@ -45,14 +27,7 @@ export function middleware (req) {
 // Supports both a single string value or an array of matchers
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     */
-    // '/((?!api|static|_next|assets|favicon.ico).*)',
-    '/(.*buildManifest.js*)',
-    '/'
+    '/:path',
+    '/(.*)'
   ]
 }
---
+++ src/middleware/handlers.js
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server'
+
+/** @type string */
+const regions = process.env.NEXT_PUBLIC_UNSUPPORTED_REGIONS || ''
+
+const unavailableTo = regions.split(',').filter((x) => !!x)
+
+/**
+ *
+ * @param {import("next/server").NextRequest} req
+ * @returns {Promise<Response | undefined> | Response | undefined}
+ */
+export function handleBuildManifest (req) {
+  if (req.url.includes('buildManifest')) {
+    const response = NextResponse.rewrite(new URL('/buildManifest.js', req.url))
+    response.headers.set('Access-Control-Allow-Origin', 'null')
+    return response
+  }
+}
+
+/**
+ *
+ * @param {import("next/server").NextRequest} req
+ * @returns {Promise<Response | undefined> | Response | undefined}
+ */
+export function handleHtmlPages (req) {
+  const isHTMLPage = typeof req.headers.get('accept') === 'string' && (req.headers.get('accept').includes('text/html') || req.headers.get('accept').includes('application/xhtml+xml'))
+
+  const country = req.geo?.country || ''
+
+  if (!isHTMLPage || !country || unavailableTo.length === 0) {
+    return
+  }
+
+  const unavailable = unavailableTo.indexOf(country) > -1
+  const landingPage = req.nextUrl.clone().pathname === '/unavailable'
+
+  if (unavailable && !landingPage) {
+    const response = NextResponse.rewrite(new URL('/unavailable', req.url), { status: 451 })
+    response.headers.set('Access-Control-Allow-Origin', 'null')
+    return response
+  }
+}
