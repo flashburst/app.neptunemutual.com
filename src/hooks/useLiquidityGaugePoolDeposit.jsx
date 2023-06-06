13ce7afdf26f24ac26985b71fd7760319649e5d2
acharyarupak391 <acharyarupak391@gmail.com>
2023-06-06 16:40:08 +0545

    updated and restructured the liquidity gauge pools components & hooks

--- src/hooks/useLiquidityGaugePoolDeposit.jsx
+++ src/hooks/useLiquidityGaugePoolDeposit.jsx
@@ -1,5 +1,4 @@
 import {
-  useCallback,
   useEffect,
   useState
 } from 'react'
@@ -16,7 +15,6 @@ import { useErrorNotifier } from '@/src/hooks/useErrorNotifier'
 import { useTokenDecimals } from '@/src/hooks/useTokenDecimals'
 import { useTokenSymbol } from '@/src/hooks/useTokenSymbol'
 import { useTxToast } from '@/src/hooks/useTxToast'
-import { contractRead } from '@/src/services/readContract'
 import { METHODS } from '@/src/services/transactions/const'
 import {
   STATUS,
@@ -30,21 +28,18 @@ import { t } from '@lingui/macro'
 import { utils } from '@neptunemutual/sdk'
 import { useWeb3React } from '@web3-react/core'
 
-export const useLiquidityGaugePoolActions = ({ stakingTokenAddress, amount, poolKey }) => {
+export const useLiquidityGaugePoolDeposit = ({ stakingTokenAddress, amount, poolKey }) => {
   const { notifyError } = useErrorNotifier()
 
   const { networkId } = useNetwork()
   const { account, library } = useWeb3React()
 
   const [approving, setApproving] = useState(false)
-  const [processingTx, setProcessingTx] = useState(false)
+  const [depositing, setDepositing] = useState(false)
 
   const liquidityGaugePoolAddress = CONTRACT_DEPLOYMENTS[networkId]?.liquidityGaugePool
   const stakingTokenSymbol = useTokenSymbol(stakingTokenAddress)
   const stakingTokenDecimals = useTokenDecimals(stakingTokenAddress)
-
-  const [poolStaked, setPoolStaked] = useState('0')
-  const [rewardAmount, setRewardAmount] = useState('0')
 
   const {
     allowance,
@@ -61,38 +56,6 @@ export const useLiquidityGaugePoolActions = ({ stakingTokenAddress, amount, pool
   useEffect(() => {
     updateAllowance(liquidityGaugePoolAddress)
   }, [updateAllowance, liquidityGaugePoolAddress])
-
-  const fetchStakedAndReward = useCallback(async () => {
-    if (!networkId || !account || !poolKey) return
-
-    try {
-      const signerOrProvider = getProviderOrSigner(library, account, networkId)
-      const instance = utils.contract.getContract(liquidityGaugePoolAddress, abis.LiquidityGaugePool, signerOrProvider)
-
-      const args = [poolKey, account]
-      const calls = [
-        contractRead({
-          instance,
-          methodName: '_poolStakedByMe',
-          args
-        }),
-        contractRead({
-          instance,
-          methodName: 'calculateReward',
-          args
-        })
-      ]
-      const [staked, reward] = await Promise.all(calls)
-      setPoolStaked(staked.toString())
-      setRewardAmount(reward.toString())
-    } catch (error) {
-      console.error('Error in getting staked pool amount & reward', error)
-    }
-  }, [account, library, liquidityGaugePoolAddress, networkId, poolKey])
-
-  useEffect(() => {
-    fetchStakedAndReward()
-  }, [fetchStakedAndReward])
 
   const handleApprove = () => {
     setApproving(true)
@@ -177,12 +140,12 @@ export const useLiquidityGaugePoolActions = ({ stakingTokenAddress, amount, pool
       return
     }
 
-    setProcessingTx(true)
+    setDepositing(true)
 
     const cleanup = () => {
       updateBalance()
       updateAllowance(liquidityGaugePoolAddress)
-      setProcessingTx(false)
+      setDepositing(false)
     }
 
     const handleError = (err) => {
@@ -269,214 +232,19 @@ export const useLiquidityGaugePoolActions = ({ stakingTokenAddress, amount, pool
       cleanup()
     }
   }
-
-  const handleWithdraw = async () => {
-    if (!account || !networkId) {
-      return
-    }
-
-    setProcessingTx(true)
-
-    const cleanup = () => {
-      updateBalance()
-      updateAllowance(liquidityGaugePoolAddress)
-      setProcessingTx(false)
-    }
-
-    const handleError = (err) => {
-      notifyError(err, t`Could not withdraw rewards`)
-    }
-
-    try {
-      const signerOrProvider = getProviderOrSigner(library, account, networkId)
-      const instance = utils.contract.getContract(liquidityGaugePoolAddress, abis.LiquidityGaugePool, signerOrProvider)
-
-      const onTransactionResult = async (tx) => {
-        TransactionHistory.push({
-          hash: tx.hash,
-          methodName: METHODS.GAUGE_POOL_WITHDRAW,
-          status: STATUS.PENDING,
-          data: { value: amount, tokenSymbol: stakingTokenSymbol }
-        })
-
-        await txToast
-          .push(
-            tx,
-            {
-              pending: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW,
-                STATUS.PENDING
-              ).title,
-              success: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW,
-                STATUS.SUCCESS
-              ).title,
-              failure: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW,
-                STATUS.FAILED
-              ).title
-            },
-            {
-              onTxSuccess: () => {
-                TransactionHistory.push({
-                  hash: tx.hash,
-                  methodName: METHODS.GAUGE_POOL_WITHDRAW,
-                  status: STATUS.SUCCESS
-                })
-              },
-              onTxFailure: () => {
-                TransactionHistory.push({
-                  hash: tx.hash,
-                  methodName: METHODS.GAUGE_POOL_WITHDRAW,
-                  status: STATUS.SUCCESS
-                })
-              }
-            }
-          )
-          .catch((err) => {
-            handleError(err)
-          })
-
-        cleanup()
-      }
-
-      const onRetryCancel = () => {
-        cleanup()
-      }
-
-      const onError = (err) => {
-        handleError(err)
-        cleanup()
-      }
-
-      const args = [poolKey, convertToUnits(amount, stakingTokenDecimals).toString()]
-      writeContract({
-        instance,
-        methodName: 'withdraw',
-        onTransactionResult,
-        onRetryCancel,
-        onError,
-        args
-      })
-    } catch (err) {
-      handleError(err)
-      cleanup()
-    }
-  }
-
-  const handleWithdrawRewards = async () => {
-    if (!account || !networkId) {
-      return
-    }
-
-    setProcessingTx(true)
-
-    const cleanup = () => {
-      updateBalance()
-      updateAllowance(liquidityGaugePoolAddress)
-      setProcessingTx(false)
-    }
-
-    const handleError = (err) => {
-      notifyError(err, t`Could not withdraw rewards`)
-    }
-
-    try {
-      const signerOrProvider = getProviderOrSigner(library, account, networkId)
-      const instance = utils.contract.getContract(liquidityGaugePoolAddress, abis.LiquidityGaugePool, signerOrProvider)
-
-      const onTransactionResult = async (tx) => {
-        TransactionHistory.push({
-          hash: tx.hash,
-          methodName: METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-          status: STATUS.PENDING
-          // data: { value: amount, tokenSymbol: stakingTokenSymbol }
-        })
-
-        await txToast
-          .push(
-            tx,
-            {
-              pending: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-                STATUS.PENDING
-              ).title,
-              success: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-                STATUS.SUCCESS
-              ).title,
-              failure: getActionMessage(
-                METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-                STATUS.FAILED
-              ).title
-            },
-            {
-              onTxSuccess: () => {
-                TransactionHistory.push({
-                  hash: tx.hash,
-                  methodName: METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-                  status: STATUS.SUCCESS
-                })
-              },
-              onTxFailure: () => {
-                TransactionHistory.push({
-                  hash: tx.hash,
-                  methodName: METHODS.GAUGE_POOL_WITHDRAW_REWARDS,
-                  status: STATUS.SUCCESS
-                })
-              }
-            }
-          )
-          .catch((err) => {
-            handleError(err)
-          })
-
-        cleanup()
-      }
-
-      const onRetryCancel = () => {
-        cleanup()
-      }
-
-      const onError = (err) => {
-        handleError(err)
-        cleanup()
-      }
-
-      const args = [poolKey, convertToUnits(amount, stakingTokenDecimals).toString()]
-      writeContract({
-        instance,
-        methodName: 'withdrawRewards',
-        onTransactionResult,
-        onRetryCancel,
-        onError,
-        args
-      })
-    } catch (err) {
-      handleError(err)
-      cleanup()
-    }
-  }
-
   const canApprove = !toBN(amount).isZero() &&
     convertToUnits(amount, stakingTokenDecimals).isLessThanOrEqualTo(balance)
-  const canSpend = !toBN(amount).isZero() &&
+  const canDeposit = !toBN(amount).isZero() &&
     convertToUnits(amount, stakingTokenDecimals).isLessThanOrEqualTo(allowance)
 
   return {
     handleApprove,
     handleDeposit,
-    handleWithdraw,
-    handleWithdrawRewards,
 
     approving,
-    processingTx,
+    depositing,
 
     canApprove,
-    canSpend,
-
-    balance,
-    poolStaked,
-    rewardAmount
+    canDeposit
   }
 }
