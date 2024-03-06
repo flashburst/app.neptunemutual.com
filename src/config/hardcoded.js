b0b2e416e08c05ded0d5de27f7f95c3447dec907
flashburst <82953782+flashburst@users.noreply.github.com>
2024-03-06 15:20:35 +0530

    Update chain configuration in hardcoded.js

--- src/config/hardcoded.js
+++ src/config/hardcoded.js
@@ -1,10 +1,11 @@
 export const CHAINS = {
-  FUJI: 43113,
+  ETHEREUM: 1,
   BSC: 56,
-  BASE_GOERLI: 84531,
-  MUMBAI: 80001,
+  POLYGON: 137,
   ARBITRUM: 42161,
-  ETHEREUM: 1
+  MUMBAI: 80001,
+  FUJI: 43113,
+  BASE_GOERLI: 84531
 }
 
 export const ChainConfig = {
@@ -68,29 +69,6 @@ export const ChainConfig = {
     cxTokenDecimals: 18,
     vaultTokenDecimals: 18
   },
-  [CHAINS.BASE_GOERLI]: {
-    npm: {
-      address: '0x4BbDc138dd105C7ddE874df7FCd087b064F7973d',
-      tokenSymbol: 'NPM',
-      tokenDecimals: 18
-    },
-    stablecoin: {
-      address: '0xbdCDBD278467b84F67AEE5737Ddc83A9C054cC29',
-      tokenSymbol: 'USDC',
-      tokenDecimals: 6
-    },
-    veNPM: {
-      address: '0x9Cfef27aC2Bed8689B89De0Ad7B30B02f5F45f9A',
-      tokenSymbol: 'veNPM',
-      tokenDecimals: 18
-    },
-    gaugeControllerRegistry: '0x3bA2F20FF5481451E37Cfc97f50250aB10CAd8eF',
-    neptuneLegends: '0xd673f97cA6DC3f807E0EAA9d0271b165C2A6d657',
-    policyProofMinter: '0xbF7176F75B73DF752F52f429AF853A5f7edBb1FA',
-    merkleProofMinter: '0x0866f9927d94a5D7072E91DcF77E407099170Bf5',
-    cxTokenDecimals: 18,
-    vaultTokenDecimals: 18
-  },
   [CHAINS.MUMBAI]: {
     npm: {
       address: '0x34F318d693f22168750aCf29E3A9421fD07E8516',
@@ -111,6 +89,29 @@ export const ChainConfig = {
     neptuneLegends: '0xee7698603CdfC6759B0c91A63859CE894537b067',
     policyProofMinter: '0x7baEfF27F6B9085374637e89D2Da0Bd0c5742997',
     merkleProofMinter: '0x6368195cD49212662FbE310520db321f5dbb6711',
+    cxTokenDecimals: 18,
+    vaultTokenDecimals: 18
+  },
+  [CHAINS.BASE_GOERLI]: {
+    npm: {
+      address: '0x4BbDc138dd105C7ddE874df7FCd087b064F7973d',
+      tokenSymbol: 'NPM',
+      tokenDecimals: 18
+    },
+    stablecoin: {
+      address: '0xbdCDBD278467b84F67AEE5737Ddc83A9C054cC29',
+      tokenSymbol: 'USDC',
+      tokenDecimals: 6
+    },
+    veNPM: {
+      address: '0x9Cfef27aC2Bed8689B89De0Ad7B30B02f5F45f9A',
+      tokenSymbol: 'veNPM',
+      tokenDecimals: 18
+    },
+    gaugeControllerRegistry: '0x3bA2F20FF5481451E37Cfc97f50250aB10CAd8eF',
+    neptuneLegends: '0xd673f97cA6DC3f807E0EAA9d0271b165C2A6d657',
+    policyProofMinter: '0xbF7176F75B73DF752F52f429AF853A5f7edBb1FA',
+    merkleProofMinter: '0x0866f9927d94a5D7072E91DcF77E407099170Bf5',
     cxTokenDecimals: 18,
     vaultTokenDecimals: 18
   }
